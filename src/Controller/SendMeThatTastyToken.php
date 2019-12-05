2b0d1fea8ce5b804eb3ae4a1e73e832bc626858c
Łukasz Pietrek <lpietrek@gmail.com>
2019-12-05 22:48:26 +0100

    Setup proper namespace and add basic test support

--- src/Controller/SendMeThatTastyToken.php
+++ src/Controller/SendMeThatTastyToken.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace Birdperson\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
--- tests/SendingTokenHappyPathTest.php
+++ tests/SendingTokenHappyPathTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests;
+namespace Birdperson\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
